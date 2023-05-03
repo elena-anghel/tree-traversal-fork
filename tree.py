b6c9207ceeaad753082912ce1f44b5bb01d00a01
Elena Anghel <elena.anghel@192.168.1.4>
2023-05-03 19:58:35 +0300

    Modified Tree.py and added test_Tree.py

--- tree.py
+++ tree.py
@@ -41,7 +41,6 @@ class Tree:
 
     def find(self, data):
         """Method for find data in the tree
-
         Args:
             data (int): data to find
 
@@ -62,27 +61,41 @@ class Tree:
             return self._find(data, node.right)
 
     def deleteTree(self):
-        # TODO 1
+        """ Method to delete the nodes from a tree """
         self.root = None
 
     def printTree(self):
-        # TODO 1
+        """ Method to print the tree using inorder traveral"""
         if self.root is not None:
             self._printInorderTree(self.root)
 
     def _printInorderTree(self, node):
-        # TODO 1
+        """ Method to traverse the tree inorder and print the nodes
+        Args:
+            node (Node): the node to start from
+        """
         if node is not None:
             self._printInorderTree(node.left)
             print(str(node.data) + ' ')
             self._printInorderTree(node.right)
 
     def _printPreorderTree(self, node):
-        # TODO 2
-        pass
+        """Method to traverse the tree preorder and print the nodes
+        Args:
+           node (Node): the node to start from
+        """
+
+        if node is not None:
+            print(str(node.data) + ' ')
+            self._printPreorderTree(node.left)
+            self._printPreorderTree(node.right)
 
     def _printPostorderTree(self, node):
-        # TODO 2
-        pass
+        """Method to traverse the tree postorder and print the nodes           Args:
+           node (Node): the node to start from
+        """
 
-
+        if node is not None:
+            self._printPreorderTree(node.left)
+            self._printPreorderTree(node.right)
+            print(str(node.data) + ' ')
